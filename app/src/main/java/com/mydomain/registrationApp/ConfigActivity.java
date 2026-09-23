27561d84efe2dde6644acead4727c991cf21fe86
Joao Vitor Lavigne <joaolavigne0@gmail.com>
2026-09-22 21:18:40 -0300

    codigo final comentado

--- app/src/main/java/com/mydomain/registrationApp/ConfigActivity.java
+++ app/src/main/java/com/mydomain/registrationApp/ConfigActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+//imports para salvar dados dps de fechar
 import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class ConfigActivity extends AppCompatActivity {
-
+//Chaves para ler as configuraões
     private static final String PREFS_NAME = "user_config_prefs";
     private static final String KEY_NAME = "key_name";
     private static final String KEY_SEX = "key_sex"; // true = masculino, false = feminino
@@ -26,7 +27,7 @@ public class ConfigActivity extends AppCompatActivity {
 
     private static final String DEFAULT_NAME = "Admin";
     private static final boolean DEFAULT_SEX_MALE = true;
-
+// referenciar os elementos
     private EditText editNome;
     private RadioGroup radioGroupSex;
     private RadioButton radioBtnMale;
@@ -49,7 +50,7 @@ public class ConfigActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+//conecta variáveis aos componentes
         editNome = findViewById(R.id.edit_nome);
         radioGroupSex = findViewById(R.id.radio_group_sex);
         radioBtnMale = findViewById(R.id.radio_btn_male);
@@ -60,15 +61,15 @@ public class ConfigActivity extends AppCompatActivity {
 
         Button btnConfirm = findViewById(R.id.btn_confirm);
         Button btnCancel = findViewById(R.id.btn_cancel);
-
+//carrega dados salvos anteriormente
         prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 
         carregarConfiguracoes();
-
+// codigo para cancelar sem salvar ou salvar
         btnConfirm.setOnClickListener(this::onConfirmClick);
         btnCancel.setOnClickListener(v -> finish());
     }
-
+// Le os dados e preenche os campos respectivos
     private void carregarConfiguracoes() {
         String nomeSalvo = prefs.getString(KEY_NAME, DEFAULT_NAME);
         boolean sexoMasculino = prefs.getBoolean(KEY_SEX, DEFAULT_SEX_MALE);
@@ -88,7 +89,7 @@ public class ConfigActivity extends AppCompatActivity {
         checkboxHipoglicidica.setChecked(hipoglicidica);
         checkboxSemGluten.setChecked(semGluten);
     }
-
+//codigo para salvar valores atuais antes de fechar a tela
     private void onConfirmClick(View view) {
         String nome = editNome.getText().toString().trim();
         boolean sexoMasculino = radioGroupSex.getCheckedRadioButtonId() == R.id.radio_btn_male;
